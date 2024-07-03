d35ee87a2b384351703b1714352afe22218221bd
enzogabrijelcic <enzogabrijelcic@gmail.com>
2024-07-03 14:39:22 -0300

    aula 02.07

--- interface_grafica/exercicio/4.py
+++ interface_grafica/exercicio/4.py
@@ -2,15 +2,16 @@
 #Faça perguntar o nome e a data de nascimento. Depois exiba isso no console.
 import tkinter as tk
 from tkinter import ttk
-nome = input('qual é o seu nome?')
-idade = input('qual é a data do seu nascimento')
+nome = input('qual é o seu nome? ')
+data_nasc = input('qual é a data do seu nascimento? ')
 root=tk.Tk()
+root.title('exercicio 4')
 root.geometry('600x600')
 
-label1 = tk.Label(root, text=f'nome:{nome}')
-label1.pack()
+label1 = tk.Label(root, text=f'nome:{nome}', font =("Arial", 14), fg = 'blue', background= 'yellow')
+label1.pack(fill='both', expand=True)
 
-label2 = tk.Label(root, text='nascimento:')
-label2.pack()  
+label2 = tk.Label(root, text=f'nascimento:{data_nasc}',font =("Arial", 14), fg = 'blue', background= 'red')
+label2.pack(fill='both', expand=True)  
     
 root.mainloop()
